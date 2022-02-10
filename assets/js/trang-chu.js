12269876339260acb7c5d786d63d559182f89a7b
ldnminh31 <ledoannhatminh31@gmail.com>
2022-02-10 21:59:19 +0700

    pc

--- assets/js/trang-chu.js
+++ assets/js/trang-chu.js
@@ -1,5 +1,48 @@
-$(function(){
-    $('.carousel').carousel({
-        interval: 4000
-      })
+$(function () {
+  $(".carousel").carousel({
+    interval: 4000,
+  });
+  initTimer();
+  function initTimer() {
+    if ($(".timer").length) {
+      // Uncomment line below and replace date
+      // var target_date = new Date("Dec 7, 2017").getTime();
+
+      // comment lines below
+      var date = new Date();
+      date.setDate(date.getDate() + 3);
+      var target_date = date.getTime();
+      //----------------------------------------
+
+      // variables for time units
+      var days, hours, minutes, seconds;
+
+      var d = $("#day");
+      var h = $("#hour");
+      var m = $("#minute");
+      var s = $("#second");
+
+      setInterval(function () {
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
+
+        // do some time calculations
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
+
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+
+        // display result
+        d.text(days);
+        h.text(hours);
+        m.text(minutes);
+        s.text(seconds);
+      }, 1000);
+    }
+  }
 });
